130494bc7f4303db849ab20efa3217064b06f820
Zachary Handel <zachary.handelzh@gmail.com>
2022-12-25 21:03:17 -0600

    Adding messages

---
+++ main.js
@@ -0,0 +1,6 @@
+const messages = ["You have given up before, are you going to do it again?", 
+                  "You are overweight and you are tired of it. YOU CAN FIX THIS!",
+                  "Stop saying TOMORROW and do it NOW!",
+                  "Who cares if you're tired? That is the only reason that you are failing.",
+                  "Take this semester by the neck and OWN IT!"];    //stored messages
+
