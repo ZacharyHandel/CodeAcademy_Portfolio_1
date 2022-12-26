1d483a469c13b756aef4dbcf76659632a6e21fcc
Zachary Handel <zachary.handelzh@gmail.com>
2022-12-25 21:06:45 -0600

    Changing message system

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const messages = ["You have given up before, are you going to do it again?", 
-                  "You are overweight and you are tired of it. YOU CAN FIX THIS!",
-                  "Stop saying TOMORROW and do it NOW!",
-                  "Who cares if you're tired? That is the only reason that you are failing.",
-                  "Take this semester by the neck and OWN IT!"];    //stored messages
+const firstWord = "You";
+const secondWord = ["will","are","can"];
+const thirdWord = ["win!", "succeed!","strong!","determined!"];
+
+
 
